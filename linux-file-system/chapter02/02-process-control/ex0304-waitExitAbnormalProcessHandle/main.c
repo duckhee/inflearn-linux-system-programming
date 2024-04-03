f9ab46c208e9b23b9d2e5816b215df0152176b55
DoukheeWon <fain9301@yahoo.com>
2024-04-04 01:58:51 +0900

    feat: add simple comment

--- linux-file-system/chapter02/02-process-control/ex0304-waitExitAbnormalProcessHandle/main.c
+++ linux-file-system/chapter02/02-process-control/ex0304-waitExitAbnormalProcessHandle/main.c
@@ -6,9 +6,15 @@
 #include <unistd.h>
 
 void term_stat(int status) {
+    /** 정상 종료에 대한 확인 */
     if (WIFEXITED(status)) {
+        /** 정상 종료 코드 값 가져오기 */
         printf("[정상 죵료] exit code : %d\r\n", WEXITSTATUS(status));
-    } else if (WIFSIGNALED(status)) {
+
+    }
+        /** 비정상 종료에 대한 signal 종료인지 확인 */
+    else if (WIFSIGNALED(status)) {
+        /** signal number 와 core dump 유무 확인 */
         printf("[비정상 종료] signal code : %d %s\r\n", WTERMSIG(status), (WCOREDUMP(status)) ? "(core dump)" : "");
     }
 }
