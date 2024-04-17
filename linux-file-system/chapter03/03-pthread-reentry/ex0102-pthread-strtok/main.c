86811c7dd1f111870d0edd9912bb4037ae6527d8
DoukheeWon <fain9301@yahoo.com>
2024-04-17 19:18:33 +0900

    feat: mutex lock add

--- linux-file-system/chapter03/03-pthread-reentry/ex0102-pthread-strtok/main.c
+++ linux-file-system/chapter03/03-pthread-reentry/ex0102-pthread-strtok/main.c
@@ -5,6 +5,8 @@
 void *my_handler(void *);
 void *ip_filter(void *);
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 /**
  *
  * 문자게 생기는 이유가 strtok 함수 안에서 static pointer 로 진행을 하기 때문에
@@ -29,6 +31,8 @@ void *my_handler(void *data) {
     char hp[] = "010-1234-5678";
     /** 잘라진 문자열을 받기 위한 포인터 변수 */
     char *p;
+    /** static 변수를 사용하지 않기 위해서 mutex_lock을 이용한 처리 */
+    pthread_mutex_lock(&mutex);
     /** 문자열 자르기 */
     p = strtok(hp, "-");
     /** 더 이상 자를 문자열이 없을 때 까지 진행하는 반복문 */
@@ -37,12 +41,16 @@ void *my_handler(void *data) {
         /** 문자열에 대한 정보를 NULL 로 넣어야 다음 위치로 이동하면서 잘라낸다. */
         p = strtok(NULL, "-");
     }
+    /** static 변수 사용이 끝났으므로 lock 해제 */
+    pthread_mutex_unlock(&mutex);
 }
 
 void *ip_filter(void *data){
     char ip[] = "127.0.0.1";
     /** 잘라진 문자열을 받기 위한 포인터 변수 */
     char *p;
+    /** static 변수를 사용하지 않기 위해서 mutex_lock을 이용한 처리 */
+    pthread_mutex_lock(&mutex);
     /** 문자열 자르기 */
     p = strtok(ip, ".");
     /** 더 이상 자를 문자열이 없을 때 까지 진행하는 반복문 */
@@ -51,4 +59,6 @@ void *ip_filter(void *data){
         /** 문자열에 대한 정보를 NULL 로 넣어야 다음 위치로 이동하면서 잘라낸다. */
         p = strtok(NULL, ".");
     }
+    /** static 변수 사용이 끝났으므로 lock 해제 */
+    pthread_mutex_unlock(&mutex);
 }
